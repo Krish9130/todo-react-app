1e1d4edea8fac7d6ce1ccc31b891449ca751b602
VIkasKumarP <vikaskumarp1061@gmail.com>
2023-10-09 14:47:03 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 cd /opt
 # navigate to app folder
 git clone https://github.com/vikaskumarp1061/todo-react-app.git
-cd todo-react-app
+cd todo-react-app/
 npm install 
 npm run build
 #npm install pm2@latest -g
