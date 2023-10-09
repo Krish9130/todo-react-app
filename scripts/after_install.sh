e360cb9f4939fc1928d4c09a21381da272bcff42
VIkasKumarP <vikaskumarp1061@gmail.com>
2023-10-09 15:31:45 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,7 @@
 cd /opt/build/
 cp -r * /var/www/html
 cd /var/www/html
-mv index.nginx-debian.html ../
+#mv index.nginx-debian.html ../
+service nginx reload
 #npm install pm2@latest -g
 #pm2 start npm --name "todo-react-app" -- start
